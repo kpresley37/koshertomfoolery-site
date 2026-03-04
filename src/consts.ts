78bfe66536e85a296310dd6a50a588d56ecc5a72
Kosher Tomfoolery <koshertomfoolerygames@gmail.com>
2026-03-03 19:03:29 -0500

    Initial KosherTomfoolery site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Kosher Tomfoolery';
+export const SITE_DESCRIPTION = 'Gaming clips, chaos, and comedy.';
